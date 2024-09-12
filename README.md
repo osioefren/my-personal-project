b6d12b83976dfe1784f0798b273649e753ecd5b5
kurohoro10 <kurohoro10@gmail.com>
2024-09-12 14:33:41 +0800

    Created a responsive grid design

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a road to a full website from scratch
+
+Create responsive grid design
+
+Next is php CRUD
--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/normalize.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
 </head>
 <body>
-    
+    <main>
+        <section id="table" class="container">
+            <div class="one"></div>
+            <div class="two"></div>
+            <div class="three"></div>
+            <div class="four"></div>
+            <div class="five"></div>
+        </section>
+    </main>
 </body>
 </html>
