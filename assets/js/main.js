0a194ab804a640ab774efe412c97411d7bd0b4d9
kurohoro10 <kurohoro10@gmail.com>
2024-09-12 16:15:35 +0800

    Design the CRUD table and added a hover effect

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const crudTable = document.querySelector('.crud-table table');
+
+    if (!crudTable) {
+        console.log("Table with ID 'crud-table' not found!");
+        return;
+    }
+
+    const highLightTableColumn = (ev, changes1, changes2) => {
+        changes1 = changes1 || '';
+        changes2 = changes2 || '';
+        crudTable.addEventListener(ev, (e) => {
+            if (e.target.tagName === 'TD' || e.target.tagName === 'TH') {
+                const columnIndex = e.target.cellIndex;
+                highlightColumn(columnIndex, changes1, changes2);
+            }
+        });
+    };
+
+    const highlightColumn = (index, color1, color2) => {
+        color1 = color1 || '';
+        color2 = color2 || '';
+        const rows = crudTable.rows;
+        if (!rows || rows.length === 0) {
+            console.error('No rows found in the table.');
+            return;
+        }
+
+        for (let row of rows) {
+            row.cells[index].style.backgroundColor = color1;
+            
+        }
+
+        const headerCell = crudTable.querySelector(`thead th:nth-child(${index + 1}) h4`);
+        if (headerCell) {
+            headerCell.style.color = color2;
+        }
+    }
+
+    highLightTableColumn('mouseover', 'var(--yellow2)', 'var(--gray1)');
+    highLightTableColumn('mouseout', '', '');
+
+});
--- index.php
+++ index.php
@@ -6,16 +6,74 @@
     <title>Document</title>
     <link rel="stylesheet" href="./assets/css/normalize.css">
     <link rel="stylesheet" href="./assets/css/main.css">
+    <!-- <link rel="stylesheet" href="./assets/css/grid.css"> -->
 </head>
 <body>
     <main>
-        <section id="table" class="container">
+        <!-- Responsive Grid Design -->
+        <!-- <section id="table" class="container">
             <div class="one"></div>
             <div class="two"></div>
             <div class="three"></div>
             <div class="four"></div>
             <div class="five"></div>
+        </section> -->
+
+        <section id="crud-table" class="crud-table container">
+            <table>
+                <thead>
+                    <tr>
+                        <th><h4>ID</h4></th>
+                        <th><h4>Name</h4></th>
+                        <th><h4>Username</h4></th>
+                        <th><h4>Email</h4></th>
+                        <th><h4>Password</h4></th>
+                        <th><h4>Action</h4></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Test</td>
+                        <td>uname</td>
+                        <td>test@mail.com</td>
+                        <td>test</td>
+                        <td>
+                            <form id="crud-table-form">
+                                <button class="crud-table-edit">
+                                    <span><i class="fa-solid fa-pen-to-square"></i></span>
+                                </button>
+
+                                <button class="crud-table-delete">
+                                    <span><i class="fa-solid fa-trash"></i></span>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Test</td>
+                        <td>uname</td>
+                        <td>test@mail.com</td>
+                        <td>test</td>
+                        <td>
+                            <form id="crud-table-form">
+                                <button class="crud-table-edit">
+                                    <span><i class="fa-solid fa-pen-to-square"></i></span>
+                                </button>
+
+                                <button class="crud-table-delete">
+                                    <span><i class="fa-solid fa-trash"></i></span>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </section>
     </main>
+
+    <script src="https://kit.fontawesome.com/61c5d3d4be.js" crossorigin="anonymous"></script>
+    <script src="./assets/js/main.js"></script>
 </body>
 </html>
